02bbb36ca3e134fff86136b6a495da21c09721d3
Jakub Janarek <jjanarek@gmail.com>
2017-11-01 12:47:33 +0100

    upgrade to es6 syntax

--- webhoseio.js
+++ webhoseio.js
@@ -1,51 +1,38 @@
-'use strict';
+const querystring = require('querystring');
+const axios = require('axios');
 
-require('isomorphic-fetch');
-require('es6-promise').polyfill();
 
-exports.config = function (options) {
-  return new WebhoseioClient(options);
-};
+config = (options) => new WebhoseioClient(options)
 
-function WebhoseioClient(options) {
-  options = options || {};
-  if (!options.token) {
-    throw new Error(`Missing required argument 'token'.`);
+module.exports = { config }
+
+class WebhoseioClient {
+  constructor(options = {}) {
+    if (!options.token) {
+      throw new Error(`Missing required argument 'token'.`)
+    }
+    this.options = {
+      token: options.token,
+      format: options.format || 'json'
+    }
   }
-  this.options = {
-    token: options.token,
-    format: options.format || 'json'
-  };
-}
 
-function getQuery(obj) {
-  const keys = Object.keys(obj);
-  if(keys.length === 0) {
-    return '';
+  async query(endpoint, params) {
+    let queryObj = querystring.stringify({...this.options, params})
+    this.nextUri = `https://webhose.io/${endpoint}${queryObj}`
+    
+    return await this.getNext()
   }
-  return '?' + keys.map(p => encodeURIComponent(p) + '=' + encodeURIComponent(obj[p])).join('&');
+
+  async getNext() {
+    let data
+    try {
+      data = await axios(this.nextUri).data
+      this.nextUri = `https://webhose.io${data.next}`
+    } catch (error) {
+      throw new Error(`fetching response ${error}`)
+    }
+
+    return await data
+  }
 }
-
-
-WebhoseioClient.prototype.query = function (endpoint, params) {
-  this.nextUri = 'https://webhose.io/' + endpoint + getQuery(Object.assign({}, this.options, params));
-  return this.getNext();
-};
-
-WebhoseioClient.prototype.getNext = function () {
-  return fetch(this.nextUri)
-    .then(response => {
-      if (response.status < 200 || response.status >= 300) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      this.nextUri = 'https://webhose.io' + data.next;
-      return data;
-    })
-    .catch(error => {
-      console.log('request failed', error);
-      return error;
-    });
-};
